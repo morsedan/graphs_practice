0b20d5d546a5302ebca57ca386b6055d32cd89b0
Dan Morse <djmthedev@gmail.com>
2020-05-01 10:25:03 -0700

    added GraphMatrix class

--- ALvMatrix.py
+++ ALvMatrix.py
@@ -8,3 +8,11 @@ class GraphList:
             "E": {"D": 1}
         }
 
+class GraphMatrix:
+    def __init__(self):
+        self.edges = [[0, 1, 0, 0, 0],
+                      [0, 0, 3, 2, 0],
+                      [0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0],
+                      [0, 0, 0, 1, 0]]
+
