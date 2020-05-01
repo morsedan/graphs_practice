683dca9329862d6fa8fe5554f95e71d0bd7fb2da
Dan Morse <djmthedev@gmail.com>
2020-05-01 10:26:30 -0700

    added Graph1List class

--- ALvMatrix.py
+++ ALvMatrix.py
@@ -16,3 +16,14 @@ class GraphMatrix:
                       [0, 0, 0, 0, 0],
                       [0, 0, 0, 1, 0]]
 
+class Graph1List:
+    def __init__(self):
+        self.vertices = {
+            "A": {"B": 1},
+            "B": {"C": 3, "D": 2, "E": 1},
+            "C": {"E": 4},
+            "D": {"E": 2},
+            "E": {"F": 3},
+            "F": {},
+            "G": {"D": 1},
+        }
